055e95e02fde79f95132eaccd41f479f086c0beb
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-27 22:57:36 -0800

    socket url change

--- src/Content.svelte
+++ src/Content.svelte
@@ -1,7 +1,7 @@
 <script context="module">
   import { writable } from "svelte/store";
   import { io } from "socket.io-client";
-  const socket = io("http://localhost:3000", {
+  const socket = io("https://browser-pets-server-production.up.railway.app/", {
     autoConnect: false,
   });
   let activated = writable(false);
